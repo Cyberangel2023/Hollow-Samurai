660d46e714298132f773770cf658f7da6ead3e30
Cyberangel2023 <143792812+Cyberangel2023@users.noreply.github.com>
2025-06-01 18:42:04 +0800

    Add files via upload

---
+++ Hollow Samurai/character_manager.h
@@ -0,0 +1,36 @@
+#ifndef _CHARACTER_MANAGER_H_
+#define _CHARACTER_MANAGER_H_
+
+#include "character.h"
+
+class CharacterManager
+{
+public:
+	static CharacterManager* instance();
+
+	Character* get_enemy()
+	{
+		return enemy;
+	}
+
+	Character* get_player()
+	{
+		return player;
+	}
+
+	void on_input(const ExMessage& mag);
+	void on_update(float delta);
+	void on_render();
+
+private:
+	static CharacterManager* manager;
+
+	Character* enemy = nullptr;
+	Character* player = nullptr;
+
+private:
+	CharacterManager();
+	~CharacterManager();
+};
+
+#endif // !_CHARACTER_MANAGER_H_
